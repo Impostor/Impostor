e289cc911f73a729595206d36ff9589e78437920
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2020-09-25 16:07:11 +0200

    Create GameKeywords.cs

---
+++ src/Impostor.Server/Net/GameKeywords.cs
@@ -0,0 +1,16 @@
+namespace Impostor.Server.Net
+{
+    public enum GameKeywords : uint
+    {
+        All = 0U,
+        English = 256U,
+        Spanish = 2U,
+        Korean = 4U,
+        Russian = 8U,
+        Portuguese = 16U,
+        Arabic = 32U,
+        Filipino = 64U,
+        Polish = 128U,
+        Other = 1U
+    }
+}
