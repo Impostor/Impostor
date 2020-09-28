7fbdbf7362e354840467a0a67f79d4c69f79436a
Michael Biggins <mike@cubecoders.com>
2020-09-28 20:37:38 +0100

    Undo previous changes

--- src/Impostor.Client/Program.cs
+++ src/Impostor.Client/Program.cs
@@ -13,24 +13,9 @@ namespace Impostor.Client
         [STAThread]
         private static void Main()
         {
-            var firstArg = Environment.GetCommandLineArgs().FirstOrDefault();
-
-            if (firstArg != null && Uri.TryCreate(firstArg, UriKind.Absolute, out var uri) && uri.Scheme.ToLower() == "impostor")
-            {
-                HandleUri(uri).GetAwaiter().GetResult();
-                return;
-            }
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
-        }
-
-        private static async Task HandleUri(Uri uri)
-        {
-            var modifier = new AmongUsModifier();
-            await modifier.SaveIp(uri.Host);
-            Process.Start("steam://launch/945360");
         }
     }
 }
