daa1f8ee022e5cb679b6fced260f2c045ae470df
Michael Biggins <mike@cubecoders.com>
2020-09-28 20:38:30 +0100

    Undo more changes because I hate git

--- src/Impostor.Client/Program.cs
+++ src/Impostor.Client/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Impostor.Client.Core;
 using Impostor.Client.Forms;
 
 namespace Impostor.Client
