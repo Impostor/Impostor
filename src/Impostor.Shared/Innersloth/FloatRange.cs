64f043767a26c72499f91428ef5b982afbd40295
AeonLucid <aeonlucid@gmail.com>
2020-10-16 07:04:33 +0200

    Added CustomNetworkTransform and VoteBanSystem

---
+++ src/Impostor.Shared/Innersloth/FloatRange.cs
@@ -0,0 +1,18 @@
+﻿using Impostor.Shared.Unity;
+
+namespace Impostor.Shared.Innersloth
+{
+    public class FloatRange
+    {
+        private readonly float _min;
+        private readonly float _max;
+
+        public FloatRange(float min, float max)
+        {
+            _min = min;
+            _max = max;
+        }
+
+        public float Lerp(float v) => Mathf.Lerp(this._min, this._max, v);
+    }
+}
---
+++ src/Impostor.Shared/Unity/Mathf.cs
@@ -0,0 +1,27 @@
+﻿namespace Impostor.Shared.Unity
+{
+    public static class Mathf
+    {
+        /// <summary>
+        ///   <para>Clamps value between 0 and 1 and returns value.</para>
+        /// </summary>
+        /// <param name="value"></param>
+        public static float Clamp01(float value)
+        {
+            if ((double) value < 0.0)
+                return 0.0f;
+            return (double) value > 1.0 ? 1f : value;
+        }
+
+        /// <summary>
+        ///   <para>Linearly interpolates between a and b by t.</para>
+        /// </summary>
+        /// <param name="a">The start value.</param>
+        /// <param name="b">The end value.</param>
+        /// <param name="t">The interpolation value between the two floats.</param>
+        /// <returns>
+        ///   <para>The interpolated float result between the two float values.</para>
+        /// </returns>
+        public static float Lerp(float a, float b, float t) => a + (b - a) * Mathf.Clamp01(t);
+    }
+}
