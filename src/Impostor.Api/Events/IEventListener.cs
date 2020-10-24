691ac5cba4db4f6ebc220d7f344bd9658db1fe20
Weirdo <git.jonas@kamsker.at>
2020-10-24 19:45:38 +0200

    Extracted IManualEventListener to its own file

--- src/Impostor.Api/Events/IEventListener.cs
+++ src/Impostor.Api/Events/IEventListener.cs
@@ -1,16 +1,6 @@
-﻿using System.Threading.Tasks;
-
-namespace Impostor.Api.Events
+﻿namespace Impostor.Api.Events
 {
     public interface IEventListener
     {
-    }
-
-    public interface IManualEventListener : IEventListener
-    {
-        public bool CanExecute<T>();
-
-        public ValueTask Execute(IEvent @event);
-        EventPriority Priority { get; set; }
     }
 }
