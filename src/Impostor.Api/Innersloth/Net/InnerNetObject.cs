b55d09de71af591367a63fb4fd9f1b87e09e8b93
AeonLucid <aeonlucid@outlook.com>
2020-10-24 05:02:24 +0200

    Add useful method

--- src/Impostor.Api/Innersloth/Net/InnerNetObject.cs
+++ src/Impostor.Api/Innersloth/Net/InnerNetObject.cs
@@ -5,6 +5,8 @@ namespace Impostor.Api.Innersloth.Net
 {
     public abstract class InnerNetObject : GameObject
     {
+        private const int HostInheritId = -2;
+
         public uint NetId { get; internal set; }
 
         public int OwnerId { get; internal set; }
@@ -16,5 +18,11 @@ namespace Impostor.Api.Innersloth.Net
         public abstract bool Serialize(IMessageWriter writer, bool initialState);
 
         public abstract void Deserialize(IClientPlayer sender, IClientPlayer? target, IMessageReader reader, bool initialState);
+
+        public bool IsOwnedBy(IClientPlayer player)
+        {
+            return OwnerId == player.Client.Id ||
+                   (OwnerId == HostInheritId && player.IsHost);
+        }
     }
 }
