e2ae918596029042dbccc110f838ade9f21e3afd
AeonLucid <aeonlucid@outlook.com>
2020-10-26 00:10:06 +0100

    Clean up client manager

---
+++ src/Impostor.Api/Innersloth/TextBox.cs
@@ -0,0 +1,10 @@
+﻿namespace Impostor.Api.Innersloth
+{
+    public static class TextBox
+    {
+        public static bool IsCharAllowed(char i)
+        {
+            return i == ' ' || (i >= 'A' && i <= 'Z') || (i >= 'a' && i <= 'z') || (i >= '0' && i <= '9') || (i >= 'À' && i <= 'ÿ') || (i >= 'Ѐ' && i <= 'џ') || (i >= 'ㄱ' && i <= 'ㆎ') || (i >= '가' && i <= '힣');
+        }
+    }
+}
---
+++ src/Impostor.Server/Net/Manager/ClientManager.Api.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Impostor.Api.Net;
+using Impostor.Api.Net.Manager;
+
+namespace Impostor.Server.Net.Manager
+{
+    internal partial class ClientManager : IClientManager
+    {
+        IEnumerable<IClient> IClientManager.Clients => _clients.Values;
+    }
+}
