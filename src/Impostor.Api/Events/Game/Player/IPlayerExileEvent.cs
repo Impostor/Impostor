05b697e0734cfe7c63a46a4f9c6fdb381c1f58f8
Saghetti0 <60995055+Saghetti0@users.noreply.github.com>
2020-10-27 16:05:09 -0700

    Create IPlayerExileEvent.cs

---
+++ src/Impostor.Api/Events/Game/Player/IPlayerExileEvent.cs
@@ -0,0 +1,10 @@
+namespace Impostor.Api.Events.Player
+{
+    /// <summary>
+    /// Called whenever a player gets exiled (voted out)
+    /// Never actually gets called, for future use.
+    /// </summary>
+    public interface IPlayerExileEvent : IPlayerEvent
+    {
+    }
+}
