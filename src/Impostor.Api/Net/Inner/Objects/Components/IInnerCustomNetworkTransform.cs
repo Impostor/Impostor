cbb2326e83248e1127510af98ffaeaaa5a8761b5
danbudworthmead <danbudworthmead@gmail.com>
2020-11-02 20:17:15 +0000

    Update IInnerCustomNetworkTransform.cs

--- src/Impostor.Api/Net/Inner/Objects/Components/IInnerCustomNetworkTransform.cs
+++ src/Impostor.Api/Net/Inner/Objects/Components/IInnerCustomNetworkTransform.cs
@@ -5,6 +5,11 @@ namespace Impostor.Api.Net.Inner.Objects.Components
 {
     public interface IInnerCustomNetworkTransform : IInnerNetObject
     {
+        /// <summary>
+        ///     Snaps the current to the given position <see cref="IInnerPlayerControl"/>.
+        /// </summary>
+        /// <param name="position">The target position.</param>
+        /// <returns>Task that must be awaited.</returns>
         ValueTask SnapTo(Vector2 position);
     }
 }
