b7c9a03ca2f13fb277dc3990e24fea6749d1f466
danbudworthmead <danbudworthmead@gmail.com>
2020-11-12 02:44:37 +0000

    Snap player to position (#109)
    * Snap player to position
    
    * Update IInnerCustomNetworkTransform.cs

--- src/Impostor.Api/Net/Inner/Objects/Components/IInnerCustomNetworkTransform.cs
+++ src/Impostor.Api/Net/Inner/Objects/Components/IInnerCustomNetworkTransform.cs
@@ -1,6 +1,15 @@
-﻿namespace Impostor.Api.Net.Inner.Objects.Components
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Impostor.Api.Net.Inner.Objects.Components
 {
     public interface IInnerCustomNetworkTransform : IInnerNetObject
     {
+        /// <summary>
+        ///     Snaps the current to the given position <see cref="IInnerPlayerControl"/>.
+        /// </summary>
+        /// <param name="position">The target position.</param>
+        /// <returns>Task that must be awaited.</returns>
+        ValueTask SnapTo(Vector2 position);
     }
 }
