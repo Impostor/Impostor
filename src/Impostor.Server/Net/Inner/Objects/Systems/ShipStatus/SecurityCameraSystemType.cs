a08fa9f0b9e95b76f545425bf28abb1677a7677d
Mr-VIT <vit@v1t.su>
2020-11-29 03:01:42 +0300

    Deserialize player Ids using Security Camera

--- src/Impostor.Server/Net/Inner/Objects/Systems/ShipStatus/SecurityCameraSystemType.cs
+++ src/Impostor.Server/Net/Inner/Objects/Systems/ShipStatus/SecurityCameraSystemType.cs
@@ -1,10 +1,11 @@
-﻿using Impostor.Api.Net.Messages;
+﻿using System.Collections.Generic;
+using Impostor.Api.Net.Messages;
 
 namespace Impostor.Server.Net.Inner.Objects.Systems.ShipStatus
 {
     public class SecurityCameraSystemType : ISystemType
     {
-        public byte InUse { get; internal set; }
+        public (HashSet<byte> UsingNow, HashSet<byte> StartedWatching, HashSet<byte> StoppedWatching) Players { get; internal set; } = (new (), new (), new ());
 
         public void Serialize(IMessageWriter writer, bool initialState)
         {
@@ -13,7 +14,20 @@ namespace Impostor.Server.Net.Inner.Objects.Systems.ShipStatus
 
         public void Deserialize(IMessageReader reader, bool initialState)
         {
-            InUse = reader.ReadByte();
+            var count = reader.ReadPackedInt32();
+            var nowInUseBy = new HashSet<byte>();
+            for (var i = 0; i < count; i++)
+            {
+                nowInUseBy.Add(reader.ReadByte());
+            }
+
+            var startedWatching = new HashSet<byte>(nowInUseBy);
+            startedWatching.ExceptWith(Players.UsingNow);
+
+            var stoppedWatching = Players.UsingNow;
+            stoppedWatching.ExceptWith(nowInUseBy);
+
+            Players = (nowInUseBy, startedWatching, stoppedWatching);
         }
     }
 }
