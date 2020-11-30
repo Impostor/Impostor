d847b5c708d88010156908759112a713b46f5dcc
Mr-VIT <vit@v1t.su>
2020-11-30 21:36:24 +0300

    Upgrade OnPlayerReportedBodyEvent to OnPlayerStartMeetingEvent (#184)
    * upgrade PlayerReportedBodyEvent to PlayerStartMeetingEvent
    
    now triggers on host player actions as well
    
    * update OnPlayerReportedBodyEvent in plugin example

--- src/Impostor.Api/Events/Game/Player/IPlayerStartMeetingEvent.cs
+++ src/Impostor.Api/Events/Game/Player/IPlayerStartMeetingEvent.cs
@@ -2,10 +2,10 @@ using Impostor.Api.Net.Inner.Objects;
 
 namespace Impostor.Api.Events.Player
 {
-    public interface IPlayerReportedBodyEvent : IPlayerEvent
+    public interface IPlayerStartMeetingEvent : IPlayerEvent
     {
         /// <summary>
-        ///     Gets the player who's body got reported.
+        ///     Gets the player who's body got reported. Is null when the meeting started by Emergency call button
         /// </summary>
         IInnerPlayerControl? Body { get; }
     }
