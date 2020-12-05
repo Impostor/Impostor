5bfd70a6f30c82432b62f62d91cc415826d93349
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-12-05 14:27:51 +0100

    Update IGamePlayerJoinedEvent.cs

--- src/Impostor.Api/Events/Game/IGamePlayerJoinedEvent.cs
+++ src/Impostor.Api/Events/Game/IGamePlayerJoinedEvent.cs
@@ -2,5 +2,9 @@
 {
     public interface IGamePlayerJoinedEvent : IGameEvent
     {
+        /// <summary>
+        ///     Gets the <see cref="IClientPlayer"/> which triggered the event.
+        /// </summary>
+        IClientPlayer Player { get; }
     }
 }
