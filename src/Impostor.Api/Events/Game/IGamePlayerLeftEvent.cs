7acf10690541d5252aa9de9f491a9d32c8a791d2
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-12-05 14:26:57 +0100

    Update IGamePlayerLeftEvent.cs

--- src/Impostor.Api/Events/Game/IGamePlayerLeftEvent.cs
+++ src/Impostor.Api/Events/Game/IGamePlayerLeftEvent.cs
@@ -2,5 +2,9 @@
 {
     public interface IGamePlayerLeftEvent : IGameEvent
     {
+        /// <summary>
+        ///     Gets the <see cref="IClientPlayer"/> which triggered the event.
+        /// </summary>
+        IClientPlayer Player { get; }
     }
 }
