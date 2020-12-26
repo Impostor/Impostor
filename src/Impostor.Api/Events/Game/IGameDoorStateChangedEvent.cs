2ade4b384f86a28bd03291da061fb9b7a4ca8d93
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2020-12-26 21:44:08 +0100

    Add files via upload

---
+++ src/Impostor.Api/Events/Game/IGameDoorStateChangedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Impostor.Api.Events
+{
+    public interface IGameDoorStateChangedEvent : IGameEvent
+    {
+        public uint DoorMask { get; } // Mask is 2^doorID e.g. for Medbay (ID: 10), it's 1024
+
+        public bool IsOpen { get; }
+    }
+}
