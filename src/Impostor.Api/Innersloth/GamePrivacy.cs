4e70a5d2018ffc5f87cad5b16a21f22edac057e0
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2020-12-30 03:06:32 +0100

    Create GamePrivacy.cs

---
+++ src/Impostor.Api/Innersloth/GamePrivacy.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Impostor.Api.Innersloth
+{
+    public enum GamePrivacy
+    {
+        Private,
+        Public,
+    }
+}
