bed46284983360a23e3c430a7812ffea8e526c81
112batman <tijnhoftijzer@gmail.com>
2021-01-08 13:28:27 +0100

    Add hostchanged event

---
+++ src/Impostor.Server/Events/Game/GameHostChangeEvent.cs
@@ -0,0 +1,19 @@
+﻿using Impostor.Api.Events;
+using Impostor.Api.Games;
+using Impostor.Api.Net;
+
+namespace Impostor.Server.Events
+{
+    public class GameHostChangeEvent : IGameHostChangeEvent
+    {
+        public GameHostChangeEvent(IGame game, IClientPlayer host)
+        {
+            Game = game;
+            Host = host;
+        }
+
+        public IGame Game { get; }
+
+        public IClientPlayer Host { get; }
+    }
+}
