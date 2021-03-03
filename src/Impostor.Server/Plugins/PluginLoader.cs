be19b1524bd77223d97ea37d704524391c28afb7
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-03-03 15:36:17 +0100

    Warn if plugin loader paths don't exist (#330)

--- src/Impostor.Server/Plugins/PluginLoader.cs
+++ src/Impostor.Server/Plugins/PluginLoader.cs
@@ -24,6 +24,8 @@ namespace Impostor.Server.Plugins
             // Add the plugins and libraries.
             var pluginPaths = new List<string>(config.Paths);
             var libraryPaths = new List<string>(config.LibraryPaths);
+            CheckPaths(pluginPaths);
+            CheckPaths(libraryPaths);
 
             var rootFolder = Directory.GetCurrentDirectory();
 
@@ -118,6 +120,17 @@ namespace Impostor.Server.Plugins
             });
 
             return builder;
+        }
+
+        private static void CheckPaths(IEnumerable<string> paths)
+        {
+            foreach (var path in paths)
+            {
+                if (!Directory.Exists(path))
+                {
+                    Logger.Warning("Path {path} was specified in the PluginLoader configuration, but this folder doesn't exist!", path);
+                }
+            }
         }
 
         private static void RegisterAssemblies(
