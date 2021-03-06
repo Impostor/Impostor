781ba34e1b363788844072b291c0063ced43e7ad
AeonLucid <aeonlucid@gmail.com>
2021-03-06 17:07:35 +0100

    Fix Impostor.Tools.ServerReplay compilation

--- src/Impostor.Api/Net/Messages/C2S/Message00HostGameC2S.cs
+++ src/Impostor.Api/Net/Messages/C2S/Message00HostGameC2S.cs
@@ -11,6 +11,12 @@ namespace Impostor.Api.Net.Messages.C2S
             writer.EndMessage();
         }
 
+        /// <summary>
+        ///     Deserialize a packet.
+        /// </summary>
+        /// <param name="reader"><see cref="IMessageReader"/> with <see cref="IMessageReader.Tag"/> 0.</param>
+        /// <param name="chatType">The chat type selected in the client of the player.</param>
+        /// <returns>Deserialized <see cref="GameOptionsData"/>.</returns>
         public static GameOptionsData Deserialize(IMessageReader reader, out ChatType chatType)
         {
             var gameOptionsData = GameOptionsData.DeserializeCreate(reader);
