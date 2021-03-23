269091986992aca5fb7c5f409b706602a9181a87
Minorusama <pitilulu@gmail.com>
2021-03-23 21:33:13 +0100

    Fix IBeforeGameCreatedEvent#TryToSetCode documentation

--- src/Impostor.Api/Events/Game/IBeforeGameCreatedEvent.cs
+++ src/Impostor.Api/Events/Game/IBeforeGameCreatedEvent.cs
@@ -16,7 +16,7 @@ namespace Impostor.Api.Events
         ///     Tries to set the game code to be used. This doesn't garantee the code will be used, if another lobby has been created with the same code before this event returns for exemple.
         /// </summary>
         /// <param name="gameCode">The code to try to set.</param>
-        /// <exception cref="ArgumentException">If the argument is not 6 characters long or does not contains only letters.</exception>
+        /// <exception cref="System.ArgumentException">If the argument is not 6 characters long or does not contains only letters.</exception>
         /// <returns>True if the code has been set. False if the code already exists.</returns>
         bool TryToSetCode(GameCode gameCode);
     }
