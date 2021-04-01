d31115096755c9b1cef72dae0aa9773ff64f995f
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-04-01 16:57:34 +0200

    API: update enums for airship

--- src/Impostor.Api/Innersloth/MapSpawn.cs
+++ src/Impostor.Api/Innersloth/MapSpawn.cs
@@ -18,6 +18,7 @@ namespace Impostor.Api.Innersloth
             [MapTypes.Skeld] = new MapSpawn(1.6f, new Vector2(-0.72f, 0.62f), new Vector2(-0.72f, 0.62f)),
             [MapTypes.MiraHQ] = new MapSpawn(1.55f, new Vector2(-4.4f, 2.2f), new Vector2(24.043f, 1.72f)),
             [MapTypes.Polus] = new MapSpawn(1f, new Vector2(16.64f, -2.46f), new Vector2(17.726f, -16.286f)),
+            [MapTypes.Airship] = new MapSpawn(0f, new Vector2(-0.66f, -0.5f), new Vector2(-0.66f, -0.5f)),
         };
 
         public float SpawnRadius { get; }
