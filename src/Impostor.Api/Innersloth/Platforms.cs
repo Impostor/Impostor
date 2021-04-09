7764987f91f5f2b20f7fc9f4ab07cf46f330c357
js6pak <kubastaron@hotmail.com>
2021-04-09 23:51:50 +0200

    Specify enum values for Platforms

--- src/Impostor.Api/Innersloth/Platforms.cs
+++ src/Impostor.Api/Innersloth/Platforms.cs
@@ -2,14 +2,14 @@ namespace Impostor.Api.Innersloth
 {
     public enum Platforms
     {
-        Unknown,
-        StandaloneEpicPC,
-        StandaloneSteamPC,
-        StandaloneMac,
-        StandaloneWin10,
-        StandaloneItch,
-        IPhone,
-        Android,
-        Switch,
+        Unknown = 0,
+        StandaloneEpicPC = 1,
+        StandaloneSteamPC = 2,
+        StandaloneMac = 3,
+        StandaloneWin10 = 4,
+        StandaloneItch = 5,
+        IPhone = 6,
+        Android = 7,
+        Switch = 8,
     }
 }
