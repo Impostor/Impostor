8d27016cb76597001cb3a3b7b1792863b338ac0d
js6pak <kubastaron@hotmail.com>
2021-04-10 23:00:34 +0200

    Fix xmldoc warnings

--- src/Impostor.Api/Extensions/DictionaryExtensions.cs
+++ src/Impostor.Api/Extensions/DictionaryExtensions.cs
@@ -5,8 +5,11 @@ namespace Impostor.Api
 {
     public static class DictionaryExtensions
     {
-        /// <summary>Returns a read-only <see cref="T:System.Collections.ObjectModel.ReadOnlyDictionary`2" /> wrapper for the dictionary.</summary>
-        /// <returns>An object that acts as a read-only wrapper around the <see cref="T:System.Collections.Generic.IDictionary`2" />.</returns>
+        /// <summary>Returns a read-only <see cref="ReadOnlyDictionary{TKey,TValue}" /> wrapper for the dictionary.</summary>
+        /// <param name="dictionary">An <see cref="IDictionary{TKey,TValue}" /> to create a <see cref="ReadOnlyDictionary{TKey,TValue}" /> from.</param>
+        /// <typeparam name="TKey">The type of the keys of <paramref name="dictionary" />.</typeparam>
+        /// <typeparam name="TValue">The type of the values of <paramref name="dictionary" />.</typeparam>
+        /// <returns>An object that acts as a read-only wrapper around the <see cref="IDictionary{TKey,TValue}" />.</returns>
         public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
             where TKey : notnull
         {
