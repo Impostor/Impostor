281414472dd8891cdd370d9420b30cd926175cf4
Harm Hoogeveen <H.Hoogeveen@student.tudelft.nl>
2021-04-11 18:14:08 +0200

    Add an optional attribute for dependencies

---
+++ src/Impostor.Api/Plugins/ImpostorDependencyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Impostor.Api.Plugins
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ImpostorDependencyAttribute : Attribute
+    {
+        public ImpostorDependencyAttribute(string name, DependencyType type)
+        {
+            Name = name;
+            DependencyType = type;
+        }
+
+        public DependencyType DependencyType { get; }
+
+        public string Name { get; }
+    }
+}
