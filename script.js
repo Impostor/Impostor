66bc8857e2d20bf7ddff37b9875032877e12206d
miniduikboot <mini@duikbo.at>
2021-06-10 20:53:31 +0200

    Site: Add support for deeplinking protocol (#414)

--- script.js
+++ script.js
@@ -33,15 +33,41 @@ async function parseAddressAsync(serverAddress) {
     throw Error(message);
 }
 
-async function downloadAsync() {
+function parseForm() {
     const serverAddress = document.getElementById("address").value;
     const serverPort = document.getElementById("port").value ?? DEFAULT_PORT;
     const serverName = document.getElementById("name").value || "Impostor";
 
+    return [serverAddress, serverPort, serverName];
+}
+
+async function downloadAsync() {
+    const [serverAddress, serverPort, serverName] = parseForm();
     const [serverIp, serverFqdn] = await parseAddressAsync(serverAddress);
-    const json = generateRegionInfo(serverName, serverIp, serverFqdn, serverPort);
-    const blob = new Blob([json], {type: "text/plain"});
+
+    const json = generateRegionInfo(
+        serverName,
+        serverIp,
+        serverFqdn,
+        serverPort
+    );
+    const blob = new Blob([json], { type: "text/plain" });
     saveFile(blob, "regionInfo.json");
+
+    return false;
+}
+
+async function openApp() {
+    const [serveraddress, serverport, servername] = parseForm();
+    const [serverip, _] = await parseAddressAsync(serveraddress);
+
+    const params = new URLSearchParams({
+        servername,
+        serverport,
+        serverip,
+    });
+    const url = `amongus://init?${params.toString()}`;
+    window.location = url;
 
     return false;
 }
@@ -49,7 +75,9 @@ async function downloadAsync() {
 let currentPlatform;
 
 function setEnabled(platform, value) {
-    document.querySelector(`.${platform}-support`).style.display = value ? "block" : "none";
+    for (const e of document.querySelectorAll(`.${platform}-support`)) {
+        e.style.display = value ? "block" : "none";
+    }
 }
 
 function setPlatform(platform) {
