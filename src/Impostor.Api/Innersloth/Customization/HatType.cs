66fb8426a6fdc0398367cac8dc207bf8d9113e5f
Bramux <52210964+Bramux@users.noreply.github.com>
2021-07-05 22:26:28 +0200

    Correct hat mismatches

--- src/Impostor.Api/Innersloth/Customization/HatType.cs
+++ src/Impostor.Api/Innersloth/Customization/HatType.cs
@@ -1,4 +1,4 @@
-﻿namespace Impostor.Api.Innersloth.Customization
+namespace Impostor.Api.Innersloth.Customization
 {
     public enum HatType : uint
     {
@@ -109,13 +109,13 @@
         ToppatHenry = 104,
         ToppatEllie = 105,
         GeoffreyPlumb = 106,
-        AngryEyebrows = 107,
-        ChocolateIce = 108,
-        HeartPin = 109,
-        Ponytail = 110,
-        RubberGlove = 111,
-        UnicornHorn = 112,
-        Zipper = 113,
-        RightHandMan = 114,
+        RightHandMan = 107,
+        AngryEyebrows = 108,
+        ChocolateIce = 109,
+        HeartPin = 110,
+        Ponytail = 111,
+        RubberGlove = 112,
+        UnicornHorn = 113,
+        Zipper = 114,
     }
 }
