e0c70869739624784483dfb955b50affee3fcf52
js6pak <kubastaron@hotmail.com>
2021-11-10 13:52:15 +0100

    RIP AnnouncementsServer

--- src/Impostor.Api/Net/Messages/AnnouncementsMessageFlags.cs
+++
@@ -1,9 +0,0 @@
-namespace Impostor.Api.Net.Messages
-{
-    public class AnnouncementsMessageFlags
-    {
-        public const byte UseCache = 0;
-        public const byte SetUpdate = 1;
-        public const byte AddLanguages = 4;
-    }
-}
