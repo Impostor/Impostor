1a767669fe2a74e2462a75f30bca46a085b5ada0
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2023-03-06 12:56:12 +0800

    Add conditional judgment

--- src/Impostor.Server/Net/State/Game.Incoming.cs
+++ src/Impostor.Server/Net/State/Game.Incoming.cs
@@ -169,7 +169,7 @@ namespace Impostor.Server.Net.State
             // Check if;
             // - The player is already in this game.
             // - The game is full.
-            if (player?.Game != this && _players.Count >= Options.MaxPlayers)
+            if (player?.Game != this && _players.Count >= Options.MaxPlayers && GameState == GameStates.NotStarted)
             {
                 return GameJoinResult.FromError(GameJoinError.GameFull);
             }
