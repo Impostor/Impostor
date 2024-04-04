96ec32db2dc7f3f6a1272ef13f6dc7b2a22b2ef8
miniduikboot <mini@duikbo.at>
2024-04-04 20:04:20 +0200

    Fix whitespace

--- src/Impostor.Api/Config/CheatingHostMode.cs
+++ src/Impostor.Api/Config/CheatingHostMode.cs
@@ -3,13 +3,13 @@ namespace Impostor.Api.Config
     /// <summary>
     /// Details if exceptions are made for hosts that are cheating.
     /// </summary>
-
     public enum CheatingHostMode
     {
         /// <summary>
         /// Hosts follow the same policies as other players.
         /// </summary>
         Never,
+
         /// <summary>
         /// Hosts are allowed to cheat if they request HostAuthority. If they
         /// do not request this, the same policies as for other players applies.
@@ -20,6 +20,7 @@ namespace Impostor.Api.Config
         /// mods and also disable server authority over MurderPlayer packets.
         /// </para>
         IfRequested,
+
         /// <summary>
         /// Hosts are always allowed to cheat.
         /// </summary>
