d00a7e859bfbbd8b201c301c996849ae9d910328
miniduikboot <mini@duikbo.at>
2024-06-20 18:14:02 +0200

    Spawn PlayerInfo's if host is not authoritive
    This PlayerInfo also needs to be synced over if it is server owned

--- src/Impostor.Server/Net/Inner/Objects/InnerGameData.cs
+++ src/Impostor.Server/Net/Inner/Objects/InnerGameData.cs
@@ -7,10 +7,13 @@ namespace Impostor.Server.Net.Inner.Objects
     internal partial class InnerGameData : IInnerGameData
     {
         private readonly ConcurrentDictionary<byte, InnerPlayerInfo> _allPlayers = new();
+        private readonly ConcurrentDictionary<int, InnerPlayerInfo> _allPlayersByClientId = new();
 
         public int PlayerCount => _allPlayers.Count;
 
         public IReadOnlyDictionary<byte, InnerPlayerInfo> Players => _allPlayers;
+
+        internal IReadOnlyDictionary<int, InnerPlayerInfo> PlayersByClientId => _allPlayersByClientId;
 
         public InnerPlayerInfo? GetPlayerById(byte id)
         {
@@ -24,12 +27,31 @@ namespace Impostor.Server.Net.Inner.Objects
 
         internal bool AddPlayer(InnerPlayerInfo playerInfo)
         {
-            return _allPlayers.TryAdd(playerInfo.PlayerId, playerInfo);
+            return _allPlayers.TryAdd(playerInfo.PlayerId, playerInfo) &&
+                _allPlayersByClientId.TryAdd(playerInfo.ClientId, playerInfo);
         }
 
         internal void RemovePlayer(byte playerId)
         {
-            _allPlayers.TryRemove(playerId, out _);
+            _allPlayers.TryRemove(playerId, out var player);
+
+            if (player != null)
+            {
+                _allPlayersByClientId.TryRemove(player.ClientId, out _);
+            }
+        }
+
+        internal byte GetNextAvailablePlayerId()
+        {
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                if (!Players.ContainsKey(i))
+                {
+                    return i;
+                }
+            }
+
+            return byte.MaxValue;
         }
     }
 }
