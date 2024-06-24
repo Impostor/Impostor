83bf1c4368ae9edc39bcb58970e5980593143fab
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-06-24 16:54:33 +0800

    Add Phantom Rpcs support

---
+++ src/Impostor.Api/Net/Messages/Rpcs/Rpc63StartVanish.cs
@@ -0,0 +1,13 @@
+namespace Impostor.Api.Net.Messages.Rpcs
+{
+    public static class Rpc63StartVanish
+    {
+        public static void Serialize(IMessageWriter writer)
+        {
+        }
+
+        public static void Deserialize(IMessageReader reader)
+        {
+        }
+    }
+}
