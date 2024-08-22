c39bc6106cf1b8f95654cdd63280625c868e295a
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-08-22 10:23:00 +0800

    Lobby behavior

---
+++ src/Impostor.Api/Net/Messages/Rpcs/Rpc61ExtendLobbyTimer.cs
@@ -0,0 +1,33 @@
+namespace Impostor.Api.Net.Messages.Rpcs
+{
+    public static class Rpc61ExtendLobbyTimer
+    {
+        public static void Serialize(IMessageWriter writer, int extensionId, bool isSuccess, byte extensionFailureReasons)
+        {
+            writer.WritePacked(extensionId);
+            writer.Write(isSuccess);
+            if (!isSuccess)
+            {
+                writer.Write(extensionFailureReasons);
+            }
+        }
+
+        public static void Deserialize(IMessageReader reader, out int extensionId, out bool isSuccess, out byte extensionFailureReasons)
+        {
+            extensionId = reader.ReadPackedInt32();
+            isSuccess = reader.ReadBoolean();
+
+            if (!isSuccess)
+            {
+                extensionFailureReasons = reader.ReadByte();
+
+                // The game currently only logs the failure reason and dont act on it.
+                // ExtensionFailureReasons enum exists in code but is not used, not adding it here until InnerSloth put real use to it.
+            }
+            else
+            {
+                extensionFailureReasons = 0;
+            }
+        }
+    }
+}
