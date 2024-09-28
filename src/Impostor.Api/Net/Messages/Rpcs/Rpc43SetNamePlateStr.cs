8069625806241a70efd27803b871d0ad21475fb8
danbudworthmead <danbudworthmead@gmail.com>
2024-09-28 17:41:50 +0100

    implemented the sequence ID

--- src/Impostor.Api/Net/Messages/Rpcs/Rpc43SetNamePlateStr.cs
+++ src/Impostor.Api/Net/Messages/Rpcs/Rpc43SetNamePlateStr.cs
@@ -2,14 +2,16 @@ namespace Impostor.Api.Net.Messages.Rpcs
 {
     public static class Rpc43SetNamePlateStr
     {
-        public static void Serialize(IMessageWriter writer, string namePlate)
+        public static void Serialize(IMessageWriter writer, string namePlate, byte nextRpcSequenceId)
         {
             writer.Write(namePlate);
+            writer.Write(nextRpcSequenceId);
         }
 
-        public static void Deserialize(IMessageReader reader, out string namePlate)
+        public static void Deserialize(IMessageReader reader, out string namePlate, out byte nextRpcSequenceId)
         {
             namePlate = reader.ReadString();
+            nextRpcSequenceId = reader.ReadByte();
         }
     }
 }
