ae7eab6bd8a4931b5de1934ed54bbb5eb09f0088
miniduikboot <mini@duikbo.at>
2024-12-27 17:23:04 +0100

    Print console log as well
    Only print it once to prevent console spam

--- src/Impostor.Server/Http/HelloController.cs
+++ src/Impostor.Server/Http/HelloController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Impostor.Server.Http;
 
@@ -8,9 +9,23 @@ namespace Impostor.Server.Http;
 [Route("/")]
 public sealed class HelloController : ControllerBase
 {
+    private static bool _shownHello = false;
+    private readonly ILogger<HelloController> _logger;
+
+    public HelloController(ILogger<HelloController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
     public IActionResult GetHello()
     {
+        if (!_shownHello)
+        {
+            _shownHello = true;
+            _logger.LogInformation("Impostor's Http server is reachable (this message is only printed once per start)");
+        }
+
         return Ok(@"Impostor is running, please configure your Among Us to connect to a game
 To generate a region file, go to https://impostor.github.io/Impostor");
     }
