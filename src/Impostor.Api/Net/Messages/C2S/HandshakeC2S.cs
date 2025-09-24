383b4b9e7df2a56c4a79055373c5034598835028
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-09-25 00:42:57 +0800

    Fix HandshakeC2S

--- src/Impostor.Api/Net/Messages/C2S/HandshakeC2S.cs
+++ src/Impostor.Api/Net/Messages/C2S/HandshakeC2S.cs
@@ -9,48 +9,15 @@ namespace Impostor.Api.Net.Messages.C2S
             clientVersion = reader.ReadGameVersion();
             name = reader.ReadString();
 
-            if (clientVersion >= Version.V1)
-            {
-                reader.ReadUInt32(); // lastNonceReceived, always 0 since 2021.11.9
-            }
+            reader.ReadUInt32(); // lastNonceReceived, always 0 since 2021.11.9
 
-            if (clientVersion >= Version.V2)
-            {
-                language = (Language)reader.ReadUInt32();
-                chatMode = (QuickChatModes)reader.ReadByte();
-            }
-            else
-            {
-                language = Language.English;
-                chatMode = QuickChatModes.FreeChatOrQuickChat;
-            }
+            language = (Language)reader.ReadUInt32();
+            chatMode = (QuickChatModes)reader.ReadByte();
 
-            if (clientVersion >= Version.V3)
-            {
-                using var platformReader = reader.ReadMessage();
-                platformSpecificData = new PlatformSpecificData(platformReader);
-                reader.ReadInt32(); // crossplayFlags, not used yet
-            }
-            else
-            {
-                platformSpecificData = null;
-            }
+            using var platformReader = reader.ReadMessage();
+            platformSpecificData = new PlatformSpecificData(platformReader);
 
-            if (clientVersion >= Version.V4)
-            {
-                reader.ReadByte(); // purpose unknown, seems hardcoded to 0
-            }
-        }
-
-        private static class Version
-        {
-            public static readonly GameVersion V1 = new(2021, 4, 25);
-
-            public static readonly GameVersion V2 = new(2021, 6, 30);
-
-            public static readonly GameVersion V3 = new(2021, 11, 9);
-
-            public static readonly GameVersion V4 = new(2021, 12, 14);
+            // Skip the rest of the message (empty friendcode and placeholder uint 0)
         }
     }
 }
