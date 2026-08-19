513b4a4a60e9ec6cfc4f15a4669abc764cebf42f
linmeideli <2558527373@qq.com>
2026-08-19 12:27:18 +0800

    Basically support AMCI

--- src/Impostor.Api/Games/IGame.cs
+++ src/Impostor.Api/Games/IGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Impostor.Api.Innersloth;
@@ -46,6 +47,11 @@ namespace Impostor.Api.Games
         /// Vanilla Among Us does not request this, but certain client-side mods will.
         /// </remarks>
         bool IsHostAuthoritive { get; }
+
+        /// <summary>
+        /// Gets the mod GUID that the game was registered with through the AMCI.
+        /// </summary>
+        Guid? ModGuid { get; }
 
         IClientPlayer? GetClientPlayer(int clientId);
 
